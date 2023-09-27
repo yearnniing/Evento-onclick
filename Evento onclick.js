6e9545208579ad16672dd0d5049001735f4bf289
yearnniing <tuemail.com>
2023-09-26 23:11:07 -0300

    tarea completa

---
+++ Evento onclick.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+function login(element) {
+    if (element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function message() {
+    alert("Ninja was liked!");
+}
